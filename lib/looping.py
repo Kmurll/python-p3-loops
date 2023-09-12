ae63596d0b77abef821137470394094767511158
Kelvin Mue <kelvinmue@gmail.com>
2023-09-12 14:48:17 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def happy_new_year():
     # code goes here!
-    pass
+    counter=10
+    while counter >= 1:
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+def reverseString(str):
+    reversedStr=""
+    for char in str:
+        reversedStr=char + reversedStr
+    return reversedStr
